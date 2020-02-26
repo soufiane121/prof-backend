85fc46c1574146ce3f909f366894b6a423dc26f1
soufiane121 <soufiane.oucherrou@gmail.com>
2020-02-25 22:53:44 -0500

    fix some bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'profolio-backend.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'smtp.gmail.com',
+    user_name:            Rails.application.credentials.gmail[:email_adrress],
+    password:             Rails.application.credentials.gmail[:email_password],
+    authentication:       'plain',
+    openssl_verify_mode: 'none',
+    enable_starttls_auto: true  }
 end
