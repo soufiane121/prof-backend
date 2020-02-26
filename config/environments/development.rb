509e75ac23b20c3c7366456d82e20ec050bb59f1
soufiane121 <soufiane.oucherrou@gmail.com>
2020-02-25 22:40:01 -0500

    fix some bugs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'profolio-backend.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
